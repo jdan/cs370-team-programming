9ea56d8f646def391da1b20b06d64444edaba297
Jordan Scales <scalesjordan@gmail.com>
2014-01-27 21:15:20 -0500

    modified permutations algorithm

--- wk1/195-anagrams/195.cpp
+++ wk1/195-anagrams/195.cpp
@@ -1,5 +1,5 @@
 /*
-* Jack Farzan
+* Jack Farzan (and Jordan Scales obv)
 * problem 195 in cpp
 */
 
@@ -10,91 +10,53 @@
 #include "stdlib.h"
 #include <list>
 
-//make-a the anagrams
-std::list<std::string> anagram2(std::string s1, std::string s2, std::list<std::string> strings)
-{
-    std::string news1, news2;
-    int i;
+/**
+ * Permutations of a given string
+ *
+ * Example permutations('abc') => ['abc', 'cba', ...];
+ *
+ * Methodology
+ * Permutation consists of:
+ *   Base case: ''
+ *     Permutations: ['']
+ *   Inductive step:
+ *     each character
+ *       appended to all of the permutations of the remaining characters
+ */
+std::list<std::string> permutations(std::string s1) {
+  // Permutations
+  // Base case: s1 is the empty string
 
-    std::cout << strings.size() << std::endl;
-    if(s1.length() == 0)
-    {
-    	strings.push_back(s2);
-    	//std::cout << s2 << std::endl;
+  std::list<std::string> result;
+  std::list<std::string> rest;
+  std::string first;
+
+  int i, j;
+
+  if (s1.length() == 0) {
+    result.push_back("");
+  } else {
+
+    for (i = 0; i < s1.length(); i++) {
+      first = s1.substr(i, 1);
+      rest = permutations(s1.substr(0, i) + s1.substr(i+1));
+
+      for (std::string item : rest) {
+        result.push_back(first + item);
+      }
     }
 
-   	for(i = 0; i < s1.length(); i++)
-   	{
-   		news1 = s1.substr(0, i) + s1.substr(i+1, s1.length());
-   		news2 = s1[i] + s2;
-   		anagram2(news1, news2, strings);
-   	}
-   	
-   	for(std::string c : strings)
-   		std::cout << c << std::endl;
+  }
 
-    return strings;
+  return result;
 }
 
-//anagram wrapper
-void anagram(std::string s)   
-{
-    std::list<std::string> strings; 
-    std::list<std::string> grams = anagram2(s, "", strings);
-    //grams.sort(); //sort the anagrams
-    //grams.unique(); //remove duplicates
-    for(std::string c : grams)
-        std::cout << c << std::endl;
-}
+int main() {
+  std::list<std::string> perms;
 
-//ok so this thing works why doesnt the other thing
+  perms = permutations("fives");
 
-/*void anagram3(std::string s1, std::string s2)
-{
-    std::string news1, news2;
-    int i;
-    if(s1.length() == 0)
-    	std::cout << s2 << std::endl;
-
-   	for(i = 0; i < s1.length(); i++)
-   	{
-   		news1 = s1.substr(0, i) + s1.substr(i+1, s1.length());
-   		news2 = s1[i] + s2;
-   		anagram3(news1, news2);
-   	}
-}*/
-
-
-int main()
-{
-    int size, i, j;
-    std::list<std::string> potato, empty, full;
-    /*std::string arr[] = {"pop", "Pufh", "kadhae", "pop"};
-    for(i = 0; i < 4; i++)
-    	potato.push_back(arr[i]);
-    potato.sort();
-    potato.unique();
-    for(std::string c : potato)
-    	std::cout << c << std::endl;*/
-
-    std::string orange = "abc";
-    //anagram3("orange", "");
-	full = anagram2(orange, "", empty);
-
-	//anagram("abc");
-	for(std::string c : full)
-		std::cout<< c << std::endl;
-	
-    /*while (scanf("%d %s", &n) > 2)
-    {
-
-		for(i=m+1;i<=n;i++) 
-		{
-		    temp = cycle(i);
-		    if (temp > max)
-			max = temp;
-		}
-		printf("%d %d %d\n",);
-    }*/
-    return(0);
+  for (std::string item : perms) {
+    std::cout << item << std::endl;
+  }
 }
