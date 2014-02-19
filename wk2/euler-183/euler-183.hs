2e91e8e36df004592601e97eafc4a873663fb7a6
Jordan Scales <scalesjordan@gmail.com>
2014-02-19 18:05:48 -0500

    euler 183 haskell solution

--- wk2/euler-183/euler-183.hs
+++ wk2/euler-183/euler-183.hs
@@ -1,14 +1,31 @@
--- gcd used to reduce fractions
--- gcd :: Integral a => a -> a -> a
--- gcd a 0 = a
--- gcd a b = gcd b $ mod a b
+-- Euler 183 Solution
+-- by Jordan Scales (for CS370)
+-- Feb 19, 2014
+-- https://projecteuler.net/problem=183
 
+-- Determines whether the given denominator of a reduced fraction leads to
+-- a terminating decimal
 terminates :: Integral a => a -> Bool
 terminates 1 = True
 terminates n
-  | (mod n 2) == 0 = terminates $ n / 2
-  | (mod n 5) == 0 = terminates $ n / 5
+  | (mod n 2) == 0 = terminates $ div n 2
+  | (mod n 5) == 0 = terminates $ div n 5
   | otherwise      = False
 
 main = do
-  putStrLn $ terminates 10
+  putStrLn $ show solution
+    where
+      -- ΣD(N) for 5 ≤ N ≤ 10000
+      solution = sum [d(i) | i <- [5..10000]]
+      -- Let D(N) = N if M(N) is a non-terminating decimal and
+      -- D(N) = -N if M(N) is a terminating decimal.
+      d n
+        | terminates m = -n
+        | otherwise    =  n
+        where
+          -- M(n) = Pmax
+          -- For our purposes, m refers to the reduced denominator of
+          --   n / k, where M(n) is normally (n/k)^k
+          -- k = round(n / e)
+          m = div pmax $ gcd pmax n
+          pmax = round $ (realToFrac n) / (exp 1)
