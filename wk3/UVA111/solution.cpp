5e52bc381084d3be282ba729856b50fc597efb22
Jordan Scales <scalesjordan@gmail.com>
2014-03-04 21:02:46 -0500

    refactor and cleanup

--- wk3/UVA111/solution.cpp
+++ wk3/UVA111/solution.cpp
@@ -1,102 +1,92 @@
+/**
+ * UVa Problem 111
+ * Natalie Barillaro and Jordan Scales
+ * 4 Mar 2014
+ */
+
 #include <cstdio>
 #include <iostream>
 
 using namespace std;
 
-/*For some strange reason things go differently when I get rid of the print statements...why is that? */
-
-
-//used for testing
-void printArray(int* arr, int s) {
-	cout << "[ ";
-	for(int i = 0; i < s; i++)
-		cout << arr[i] << " ";
-	cout << "]" << endl;
-}
+int longestIncreasingSubsequence(int*, int);
 
 int main() {
-	int i, j, max, greaterMax, n, x;
-	int* order;
-	int* submission;
-	int* records;
-	order = new int[n];
-	submission = new int[n];
-	records = new int[n];
+  int n, i, x;
+	int *order, *submission;
+
+  /* Read n first! */
 	cin >> n;
 
-	//Setting the order of the numbers.
-	//The numbers themselves are not stored in an array, but their positions. 
-	//For example, if the number 7 comes third in the order, order[7] = 3
-	//Only arrays go from index 0 to n-1, not 1-n, so we have an offset of -1
-	//So in fact, order[6] = 3, which tells us that n (6+1) is in the third place
-	for (i= 0; i < n; i++) {
-		cin >> x;
-		x--;
-		order[x] = i;
+	order = new int[n+1];
+	submission = new int[n+1];
 
+	/* Establish an "order" array */
+  for (i = 1; i < n+1; i++) {
+		cin >> x;
+		order[i] = x;
 	}
 
-	//do this for many things
+  /* Loop to read individual lines */
 	while (!cin.eof()) {
 
-		//add all of the numbers (real numbers this time) to the submission array
-		for(i=0; i < n; i++) {
-			cin >> submission[i];
-		}
-	
-
-		//cout << "Submission: "; 
-		//printArray(submission,n);
-		
-		//records tells you the largest substring at that position in the submission array
-		//since submission[0] is the first number, records[0] is one
-		records[0] = 1;
-
-		//this is the length of the largest substring overall.
-		//We have only seen one substring so far, and that is of length 1
-		greaterMax = 1;
-
-
-		//the first for loop traverses through the array starting at the second element
-		for (i = 1; i < n; i++) {
-			cout << "i: " << i << endl;
-			cout << "Number:" << submission[i] << endl;
-
-			//local max
-			max = 1;
-
-			//the second for loop goes backwards through the array, starting at submission[i-1]
-			for (j = i-1; j >= 0; j--) {
-
-				cout << "    Number Read: " << submission[j] << endl;
-				cout << "    order of i: " << order[submission[i]-1] << "order of j: "<< order[submission[j]-1]<<endl;
-				//if the number at i has a larger order than the number at position j
-				//and if 1 + the record at position j is greater than local max
-				//set 1 + record[j] to local max and keep searching for a higher max
-				//the reason for the i-1 and j-1 is the offset described before
-				if (order[submission[i]-1] > order[submission[j]-1]) {
-					if ( 1 + records[j] > max) {
-						max = records[j] + 1;
-					}
-					
-				}
-				
-			}
-
-			//store max in records[i]
-			//check to see if that max is the largest max we've seen so far
-			records[i] = max;
-			printArray(records, i+1);
-			if (records[i] > greaterMax) {
-				greaterMax = records[i];
-			}
+    /**
+     * Loop i from 1 to n (including)
+     * Read in the next number `x`
+     * The `x` slot of `submission` corresponds to the value in the `i` slot
+     *   of our original `order` array
+     */
+		for(i = 1; i < n+1; i++) {
+      cin >> x;
+      submission[x] = order[i];
 		}
 
-		cout << greaterMax << endl;
-	}
+    /* Compute the LIS and print */
+    cout << longestIncreasingSubsequence(submission+1, n) << endl;
+  }
+}
 
+/**
+ * Computes the LIS of a given array of integers
+ * Pass this an array and a length
+ */
+int longestIncreasingSubsequence(int* arr, int n) {
+  /* This is what we will use DP on */
+  int *lis = new int[n];
 
-		
+  /* Counters and a max */
+  int i, j, max;
 
+  /* Initialize LIS values - each index starts with 1! */
+  for (i = 0; i < n; i++) {
+    lis[i] = 1;
+  }
 
+  /**
+   * Bottoms up
+   * Give ourselves two counters, i and j (j < i) and traverse thru lis[]
+   * If we satisfy both of the following conditions
+   * - arr[i] > arr[j]      (i -> j is increasing)
+   * - lis[j] + 1 > lis[i]  (the LIS value at i should reflect j's (+ 1))
+   * Then we set the value of lis[i] to lis[j] + 1, since at this point
+   *   lis[i] should be a step up from lis[j]
+   */
+  for (i = 1; i < n; i++) {
+    for (j = 0; j < i; j++) {
+      if (arr[i] > arr[j] && lis[j] + 1 > lis[i]) {
+        lis[i] = lis[j] + 1;
+      }
+    }
+  }
+
+  /**
+   * Find the max in lis[], which will be the length of our LIS
+   */
+  for (i = 0, max = 0; i < n; i++) {
+    if (max < lis[i]) {
+      max = lis[i];
+    }
+  }
+
+  return max;
 }
