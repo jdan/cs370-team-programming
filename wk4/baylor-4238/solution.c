d2fea7eeba379f7fbdc4aaa7d14d4c03936ad0dc
Natalie Barillaro <Natalie@Natalies-iMac.local>
2014-04-02 08:53:59 -0400

    Solution attempt

--- wk4/baylor-4238/solution.c
+++ wk4/baylor-4238/solution.c
@@ -10,27 +10,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct point_s {
-  int x;
-  int y;
-  int z;
-} t_point;
-
-t_point *new_point(int x, int y, int z) {
-  t_point* point = (t_point*)malloc(sizeof(t_point));
-
-  point->x = x;
-  point->y = y;
-  point->z = z;
-
-  return point;
-}
-
 int main(int argc, char **argv) {
-    int i, j, k, nPoints, cases;
+    int i, j, k, nPoints, cases, sArea;
     int x, y, z;
-    t_point** points;
-
+    int badBlockIndex, goodBlockFound;
+ 
     /* Read in the number of cases */
     scanf("%d", &cases);
 
@@ -38,18 +22,80 @@ int main(int argc, char **argv) {
       /* Read in the number of points */
       scanf("%d", &nPoints);
 
-      points = (t_point**)malloc(nPoints * sizeof(t_point*));
+      /* Allocate space for the array:
+         Size of Number of Points  */
+      int ***points;
+      points = (int***)malloc(nPoints * sizeof(int**));
+      for (j = 0; j < nPoints; j++) {
+        points[j] = (int**)malloc(nPoints * sizeof(int*));
+        for (k = 0; k < nPoints; k++) {
+          points[j][k] = (int*)malloc(nPoints * sizeof(int));
+        }
+      }
+
+      /* The surface area of one block is 6 */
+      sArea = 6;
+
+      /* Index to keep track of a block that doesn't
+         share a face with another block */
+      badBlockIndex = 2;
+
+      /* Store first point */
+      scanf("%d,%d,%d", &x, &y, &z);
+      points[x][y][z] = 1;
 
       /* Load the points */
-      for (j = 0; j < nPoints; j++) {
+      for (j = 1; j < nPoints; j++) {
         scanf("%d,%d,%d", &x, &y, &z);
-        points[j] = new_point(x, y, z);
+        points[x][y][z] = 1;
+
+        sArea += 6;
+
+        goodBlockFound = 0;
+
+        /* Check for neighbors - is there a better way to do this?
+           Subtract 2 to surface area for every neighbor */
+        if (points[x+1][y][z] == 1) {
+          goodBlockFound = 1;
+          sArea -= 2;
+        }
+        if (points[x-1][y][z] == 1) {
+          goodBlockFound = 1;
+          sArea -= 2;
+        }
+        if (points[x][y+1][z] == 1) {
+          goodBlockFound = 1;
+          sArea -= 2;
+        }
+        if (points[x][y-1][z] == 1) {
+          goodBlockFound = 1;
+          sArea -= 2;
+        }
+        if (points[x][y][z+1] == 1) {
+          goodBlockFound = 1;
+          sArea -= 2;
+        }
+        if (points[x][y][z-1] == 1) {
+          goodBlockFound = 1;
+          sArea -= 2;
+        }
+        if (!goodBlockFound)
+          break;
+
+        /* Increase badBlockIndex */
+        badBlockIndex++;
       }
 
-      for (j = 0; j < nPoints; j++) {
-        printf("%d-%d-%d ", points[j]->x, points[j]->y, points[j]->z);
+      if (goodBlockFound) {
+        printf("%d %d", i+1, sArea);
       }
+
+      else {
+        printf("%d NO %d", i+1, badBlockIndex);
+      }
+
       printf("\n");
+
     }
 
     /* free points (and each point) */
